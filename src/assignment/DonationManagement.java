8b46b162d7cd1821c37ce9ece1cf0ae0339a00cf
TanJeeSchuan <tanjeeschuan@gmail.com>
2024-08-28 10:17:30 +0800

    Initial commit

---
+++ src/assignment/DonationManagement.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment;
+
+/**
+ *
+ * @author Tan Jee Schuan
+ */
+public class DonationManagement {
+    //adt<event, adt<donor, donation>>
+    //adt<cause, adt<donor, donation>>
+    
+    //set of donors
+    
+    //most recent donor //CircularFifoQueue
+    //queue
+    
+    //eg list(dict{cause, dict{donor, float donation}})
+}
---
+++ src/assignment/Donor.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment;
+
+/**
+ *
+ * @author Tan Jee Schuan
+ */
+public class Donor {
+    String name;
+    //etc
+    //dict
+}
