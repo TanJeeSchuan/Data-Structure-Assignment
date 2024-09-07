29b2872608144d9988bee9f1c43434ee11089794
TanJeeSchuan <tanjeeschuan@gmail.com>
2024-09-07 11:29:20 +0800

    Update SortedArrayList.java

--- src/adt/SortedArrayList.java
+++ src/adt/SortedArrayList.java
@@ -13,23 +13,33 @@ import java.util.Iterator;
  * @param <T>
  */
 
-public class SortedArrayList<T extends Comparable<T>> extends ArrayList{
+public class SortedArrayList<T extends Comparable<T>>{
     private Comparator<T> comparator;
     
+    protected T[] array;
+    protected static final int DEFAULT_CAPACITY = 2;
+    protected int maxSize;
+    protected int entries = 0;
+    
     public SortedArrayList(){
-        super();
+        array = (T[]) new Object[DEFAULT_CAPACITY];
+        maxSize = DEFAULT_CAPACITY;
     }
     
     public SortedArrayList(Comparator<T> comparator){
-        super();
+        array = (T[]) new Object[DEFAULT_CAPACITY];
+        maxSize = DEFAULT_CAPACITY;
         this.comparator = comparator;
     }
     
     public SortedArrayList(Object[] arr){
-        super(arr);
+        while (maxSize < arr.length) {
+            maxSize *= DEFAULT_CAPACITY;
+        }
+        array = (T[]) new Object[maxSize];
+        System.arraycopy(arr, 0, array, 0, arr.length);
     }
     
-    @Override
     public Iterator<T> iterator() {
         return new Iterator() {
             int index = 0;
@@ -48,7 +58,6 @@ public class SortedArrayList<T extends Comparable<T>> extends ArrayList{
         };
     }
     
-    @Override
     public boolean add(Object object) {
         if(isFull())
             expandArray();
@@ -62,20 +71,99 @@ public class SortedArrayList<T extends Comparable<T>> extends ArrayList{
         return true;
     }
     
-    @Override
+    private boolean add(int index, Object object) {
+        if (isFull()) {
+            expandArray();
+        }
+
+        if (index == entries) {
+            array[entries] = (T) object;
+            entries++;
+        } else {
+            makeSpace(index);
+            array[index] = (T) object;
+        }
+        return true;
+    }
+    
     public boolean contains(Object object) {
         if(comparator == null)
             return binarySearch(array, 0, entries, (T)object) != -1;
         return comparatorBinarySearch(array, 0, entries, (T)object, this.comparator) != -1;
     }
     
-    @Override
     public int indexOf(Object object){
         if(comparator == null)
             return binarySearch(array, 0, entries, (T)object);
         
         return comparatorBinarySearch(array, 0, entries, (T)object, this.comparator);
     }
+    
+        protected void shiftArray(int empty) {
+        for (int i = empty; i < entries; i++) {
+            array[i] = array[i + 1];
+        }
+    }
+
+    protected void expandArray() {
+        T[] newArr = (T[]) new Object[maxSize * DEFAULT_CAPACITY];
+
+        for (int i = 0; i < maxSize; i++) {
+            newArr[i] = (T) array[i];
+        }
+
+        maxSize *= DEFAULT_CAPACITY;
+        array = newArr;
+    }
+
+    protected void makeSpace(int index) {
+        if (entries + 1 > maxSize) {
+            expandArray();
+        }
+
+        for (int i = entries - 1; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+
+        entries++;
+        array[index] = null;
+    }
+    
+    public boolean isFull() {
+        return (entries == maxSize);
+    }
+    
+    public void clear(){
+        for(int i = 0; i < entries; i++){
+            array[i] = null;
+        }
+        entries = 0;
+    }
+    
+    public boolean isEmpty() {
+        return (entries == 0);
+    }
+
+    public Object[] toArray() {
+        Object[] output = new Object[entries];
+        System.arraycopy(array, 0, output, 0, entries);
+        return output;
+    }
+    
+    @Override
+    public String toString() {
+        String str = "[";
+        for(int i = 0; i < entries; i++){
+            str += (array[i]).toString();
+            
+            if(i < entries-1)
+                str += ", ";
+        }
+        
+        str += "]";
+        return str;
+    }
+    
     
     private int binarySearch(Object arr[], int start, int end, Object x){
         if(end > start){
